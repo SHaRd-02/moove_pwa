8734c27a886e3b19bd86251add7db473499c20a2
Ricardo <ricaroherniag@gmail.com>
2025-02-20 12:34:36 -0800

    pwa creation for mooove

---
+++ app.js
@@ -0,0 +1,35 @@
+// DOM elements
+// nav buttons
+const searchSectionBtn = document.getElementById('search-section-btn');
+const favoritesSectionBtn = document.getElementById('favorites-section-btn');
+const accountSectionBtn = document.getElementById('account-section-btn');
+// sections elements
+const accountSectionEle = document.getElementById('account-section');
+const favoritesSectionEle = document.getElementById('favorites-section');
+const searchSectionEle = document.getElementById('search-section');
+
+// Function to show a section
+function showSection(selected) {
+    // Select all elements with the class "sections" (plural)
+    let sections = document.querySelectorAll('.sections');
+    console.log(sections);
+    // Hide all sections
+    sections.forEach(section => section.classList.add('hidden'));
+
+    // Show the selected section
+    selected.classList.remove('hidden');
+}
+
+// Event listener for nav button
+accountSectionBtn.addEventListener('click', () => {
+    showSection(accountSectionEle);
+});
+
+favoritesSectionBtn.addEventListener('click', () => {
+    showSection(favoritesSectionEle);
+});
+
+searchSectionBtn.addEventListener('click', () => {
+    showSection(searchSectionEle);
+});
+
---
+++ sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"cebc38601959e894dceaabc68e7165e9","url":"app.js"},{"revision":"4b736711cae7aafac0920c64adf83d9a","url":"index.html"},{"revision":"f1c9174ba0a14d4f592ca07ba7ae0187","url":"styles.css"}]);
