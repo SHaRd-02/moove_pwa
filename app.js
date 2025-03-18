f62ae408737b78aea8c820dec7cedf3266c73992
Ricardo <ricaroherniag@gmail.com>
2025-03-18 12:27:42 -0700

    Added api key for maps

--- app.js
+++ app.js
@@ -20,6 +20,39 @@ function showSection(selected) {
     selected.classList.remove('hidden');
 }
 
+
+// google maps map
+
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 32.5245, lng: -116.9742 }, // Coordenadas de la UABC Valle de las Palmas
+        zoom: 12,
+    });
+
+    fetch("http://127.0.0.1:5000/routes")
+        .then(response => response.json())
+        .then(routes => {
+            routes.forEach(route => {
+                let path = JSON.parse(route.waypoints);
+                let routePath = new google.maps.Polyline({
+                    path: path,
+                    geodesic: true,
+                    strokeColor: "#FF0000",
+                    strokeOpacity: 1.0,
+                    strokeWeight: 2
+                });
+                routePath.setMap(map);
+            });
+        })
+        .catch(error => console.error("Error cargando rutas:", error));
+}
+
+document.addEventListener("DOMContentLoaded", initMap);
+
+
+
 // Event listener for nav button
 accountSectionBtn.addEventListener('click', () => {
     showSection(accountSectionEle);
